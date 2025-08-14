4aa7e88c26a841227ec7f77824f1a4239203d395
DinethSheveen <fdineth4000@gmail.com>
2025-08-14 16:16:09 +0100

    Initialized all the functions needed for the application

---
+++ script.js
@@ -0,0 +1,13 @@
+const task = document.querySelector(".task-input")
+const date = document.querySelector(".date-input")
+const todoSection = document.querySelector(".todo-list")
+
+let todoList = [] 
+
+function addTask(){}
+ 
+
+function renderTask(){}
+
+
+function deleteTask(){}
